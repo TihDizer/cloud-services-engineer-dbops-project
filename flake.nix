f0d6b21f312d3898b704cf0f2a00afe3714cf3d3
tihdizer <tihdizer@gmail.com>
2026-04-26 20:04:23 +0300

    feat: add docker socket

--- flake.nix
+++ flake.nix
@@ -18,8 +18,27 @@
           clear
           sudo docker compose up --detach
           echo "Nix dev shell activated"
+
+          DOCKER_SOCK="./docker-sock/docker.sock"
+          DOCKER_DATA="./docker-data"
+
+          mkdir -p "$DOCKER_DATA" ./docker-sock
+
+          dockerd \
+            --data-root="$DOCKER_DATA" \
+            --host="unix://$DOCKER_SOCK" \
+            >./dockerd.log 2>&1 &
+
+            while ! [ -S "$DOCKER_SOCK" ]; do
+              sleep 0.1
+            done
+
+            export DOCKER_HOST="unix://$PWD/$DOCKER_SOCK"
+
           trap '
             sudo docker compose down
+            kill %1
+            rm -rf ./docker-data ./docker-sock ./dockerd.log
             deactivate > /dev/null;
             echo "Nix dev shell deactivated"
           ' EXIT
